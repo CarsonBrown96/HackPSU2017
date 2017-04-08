9eaf9292b6861a01254c2b064a0f56299b199aaf
Carson Brown <carsonbrown67@gmail.com>
2017-04-08 16:50:25 -0400

    Added ability to add calories to a user

--- user.py
+++ user.py
@@ -6,19 +6,29 @@ class User:
 		self.uid = uid
 		self.calorie_goal = calorie_goal
 
-
 def getUser(message):
 	return getUserInfo(getUID(message))
 
 def getUserInfo(id):
 	filename = 'users.json'
-	with open(filename, 'rw') as f:
+	with open(filename, 'r') as f:
 		data = json.load(f)
 		for user in data['users']:
 			if user['id'] == id:
 				return user['id'], user['caloriesG'], user['caloriesT']
 		f['users'].push({"id":id, "caloriesG":"0", "caloriesT": "0"})
 		return id, "0", "0"
+
+def addCalories(cal, id):
+	filename = 'users.json'
+	with open(filename, 'r') as f:
+		data = json.load(f)
+		for user in data['users']:
+			if user['id'] == id:
+				user['caloriesT'] = int(user['caloriesT']) + cal
+	with open(filename, 'w') as f:
+		f.write(json.dumps(data))
+
 
 if __name__ == "__main__":
 	getUser(1234)
