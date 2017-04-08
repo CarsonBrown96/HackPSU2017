8ba0dc67ab5d1dbfeb8beb3fda31b1fbcd1d01dd
Peter Meglis <petermeglis@gmail.com>
2017-04-08 19:47:02 -0400

    Added response
    Bot responds with summary after user's input.

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 import os, sys
 from flask import Flask, request
+from pymessenger import Bot
+import decoder
+import response
+import split_text
+import nutritionseach
 
 app = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = ""
+
+bot = Bot(PAGE_ACCESS_TOKEN)
 
 @app.route('/', methods=['GET'])
 def verify():
@@ -15,13 +24,19 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 	data = request.get_json()
-	log(data)
+	
+	userid = decoder.get_userid(data)
+	meals = nutritionseach.get_meals(decoder.get_text(data))
+
+	respond = response.getResponse(userid, meals)
+
+	bot.send_text_message(userid, respond)
 
 	return "ok", 200
 
-def log(message):
-	print(message)
-	sys.stdout.flush()
+# def log(message):
+# 	print(message)
+# 	sys.stdout.flush()
 
 if __name__ == "__main__":
 	app.run(debug = True, port = 5000)
