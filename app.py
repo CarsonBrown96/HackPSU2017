f91805e213caa0a988389bf292e1329f89ff3dbe
Peter Meglis <petermeglis@gmail.com>
2017-04-09 09:24:20 -0400

    Test version 2.0 of app.py

--- app.py
+++ app.py
@@ -1,55 +1,29 @@
-import os, sys
 from flask import Flask, request
-from pymessenger import Bot
+import requests
 
 app = Flask(__name__)
 
-PAGE_ACCESS_TOKEN = "EAAECxaoUf2UBAL987CccjNQxbpomeZCoPeaVCzFDKgbEUfwPhWaUmuMTGmylUiC3wXZCFbe5sNAbO5IBbl5onfftWQAPNMsUeGDpBACcKpeZAdU653TFnGSzvXIYmVLEYxNv69gTtaCTOP6azs4Sa7nPhq0WaO1LUNkbOfLPgZDZD"
-
-bot = Bot(PAGE_ACCESS_TOKEN)
+ACCESS_TOKEN = "EAAECxaoUf2UBAL987CccjNQxbpomeZCoPeaVCzFDKgbEUfwPhWaUmuMTGmylUiC3wXZCFbe5sNAbO5IBbl5onfftWQAPNMsUeGDpBACcKpeZAdU653TFnGSzvXIYmVLEYxNv69gTtaCTOP6azs4Sa7nPhq0WaO1LUNkbOfLPgZDZD"
 
 
-@app.route('/', methods=['GET'])
-def verify():
-	# Webhook verification
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == "hello":
-            return "Verification token mismatch", 403
-        return request.args["hub.challenge"], 200
-    return "Hello world", 200
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
 
 
 @app.route('/', methods=['POST'])
-def webhook():
-	data = request.get_json()
-	log(data)
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    reply(sender, message[::-1])
 
-	if data['object'] == 'page':
-		for entry in data['entry']:
-			for messaging_event in entry['messaging']:
-
-				# IDs
-				sender_id = messaging_event['sender']['id']
-				recipient_id = messaging_event['recipient']['id']
-
-				if messaging_event.get('message'):
-					# Extracting text message
-					if 'text' in messaging_event['message']:
-						messaging_text = messaging_event['message']['text']
-					else:
-						messaging_text = 'no text'
-
-					# Echo
-					response = messaging_text
-					bot.send_text_message(sender_id, response)
-
-	return "ok", 200
+    return "ok"
 
 
-def log(message):
-	print(message)
-	sys.stdout.flush()
-
-
-if __name__ == "__main__":
-	app.run(debug = True, port = 80)
+if __name__ == '__main__':
+    app.run(debug=True)
