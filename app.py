0aa1254bf7b521489d8fb4de29d588ea062ee60f
jakecimba <jakecimbalista@gmail.com>
2017-04-09 10:15:23 -0400

    Find dat shit

--- app.py
+++ app.py
@@ -1,7 +1,43 @@
 import os, sys
 from flask import Flask, request
-from pymessenger import Bot
+from pymessenger.bot import Bot
+import nutritionsearch, decoder, response
 
+
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+ACCESS_TOKEN = "EAAECxaoUf2UBAL987CccjNQxbpomeZCoPeaVCzFDKgbEUfwPhWaUmuMTGmylUiC3wXZCFbe5sNAbO5IBbl5onfftWQAPNMsUeGDpBACcKpeZAdU653TFnGSzvXIYmVLEYxNv69gTtaCTOP6azs4Sa7nPhq0WaO1LUNkbOfLPgZDZD"
+
+
+def reply(user_id, msg):
+    data = {
+        "recipient": {"id": user_id},
+        "message": {"text": msg}
+    }
+    resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
+    print(resp.content)
+
+
+@app.route('/', methods=['POST'])
+def handle_incoming_messages():
+    data = request.json
+    sender = data['entry'][0]['messaging'][0]['sender']['id']
+    message = data['entry'][0]['messaging'][0]['message']['text']
+    respond = response.getResponse(sender, nutritionsearch.get_meals(message))
+    reply(sender, respond)
+
+    return "ok"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+"""
 app = Flask(__name__)
 
 PAGE_ACCESS_TOKEN = "EAAECxaoUf2UBAL987CccjNQxbpomeZCoPeaVCzFDKgbEUfwPhWaUmuMTGmylUiC3wXZCFbe5sNAbO5IBbl5onfftWQAPNMsUeGDpBACcKpeZAdU653TFnGSzvXIYmVLEYxNv69gTtaCTOP6azs4Sa7nPhq0WaO1LUNkbOfLPgZDZD"
@@ -21,6 +57,7 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
+
 	data = request.get_json()
 	log(data)
 
@@ -40,16 +77,19 @@ def webhook():
 						messaging_text = 'no text'
 
 					# Echo
-					response = messaging_text
-					bot.send_text_message(sender_id, response)
+					meals = nutritionsearch.get_meals(messaging_text)
+
+					respond = response.getResponse(sender_id, meals)
+
+					bot.send_text_message(sender_id, messaging_text)
 
 	return "ok", 200
 
 
 def log(message):
-	print(message)
 	sys.stdout.flush()
 
 
 if __name__ == "__main__":
-	app.run(debug = True, port = 80)
+	app.run(debug = True, port = 5000)
+"""
--- nutritionsearch.py
+++ nutritionsearch.py
@@ -1,12 +1,12 @@
 import requests, json
 
 def split_foods(text):
-    print(text)
     words = text.split(':')[1]
     return split_text(words)
 
 def split_text(text):
     words = [x.strip() for x in text.split(',')]
+    print("This is string:", words)
     return caloric_values(words)
 
 
@@ -15,14 +15,17 @@ def caloric_values(foods):
 	input_calorie = []
 	for input_string in foods:
 		url_format = input_string.replace(' ', '%20')
-		r = requests.get("https://api.nutritionix.com/v1_1/search/%s?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=3f8d7092&appKey=fe9f67745b9e3756438f5d6edf6f6a03"%(url_format))
-		j = r.json()
-		caloric_value = j['hits'][0]['fields']['nf_calories']
-		input_calorie.append(caloric_value)
+		print(url_format)
+		url = "https://api.nutritionix.com/v1_1/search/%s?results=0:20&fields=item_name,brand_name,item_id,nf_calories&appId=3f8d7092&appKey=fe9f67745b9e3756438f5d6edf6f6a03"%(url_format)
+		print(url)
+		r = requests.get(url)
+		print("This is status code: ", r.status_code)
+		if r.status_code == 200:
+			j = r.json()
+			caloric_value = j['hits'][0]['fields']['nf_calories']
+			input_calorie.append(caloric_value)
 	return list(zip(foods, input_calorie))
 
 def get_meals(string):
 	return split_foods(string)
-
-print(get_meals("I ate: apple, peanut butter, bacon"))
 
